a5098bf55f3b63ebc4ed0b8e2afa9415dbdb7f0d
shamikulamin <shamikulamin@gmail.com>
2018-11-15 16:07:04 -0600

    comm

--- ERS/src/main/java/com/revature/controllers/DispatcherServlet.java
+++ ERS/src/main/java/com/revature/controllers/DispatcherServlet.java
@@ -21,7 +21,7 @@ public class DispatcherServlet extends HttpServlet{
 	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:5500");
+		resp.addHeader("Access-Control-Allow-Origin", "http://villianpub-env.f2uxinfeen.us-east-2.elasticbeanstalk.com");
 //		resp.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
 		resp.addHeader("Access-Control-Allow-Headers",
 				"Origin, Methods, Credentials, X-Requested-With, Content-Type, Accept");
